bc86694265cbc8ea56bcbc072ed8c68070e0f4dd
Amber Donnelly <ambdonnelly12@gmail.com>
2019-10-17 08:55:21 -0500

    Add object literal & event listener.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 //Travel calculator that divides distance by average MPH to
 //obtain the time traveled.
+
+const calculator = {
+  divide(a, b) {
+    return a / b;
+  }
+};
+
+const form = document.querySelector("form");
+const distance = document.querySelector("#distance");
+const mph = document.querySelector("#mph");
+const output = document.querySelector("output");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
