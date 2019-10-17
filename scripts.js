4961d8e225d3a480276f23749f63a5fd1e7ee53e
Amber Donnelly <ambdonnelly12@gmail.com>
2019-10-17 08:43:32 -0500

    Set up input boxes & submit button.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,2 @@
-// Enter a description of this program here.
-
-/**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
- */
+//Travel calculator that divides distance by average MPH to
+//obtain the time traveled.
